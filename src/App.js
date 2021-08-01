702ece39f1de6c057b2a7ea1e0b9723b7ce3a31e
rajesh <coding3601@gmail.com>
2021-08-01 19:43:33 +0530

    fixed some bugs

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React, { useState, useEffect } from "react";
+import React, { useState } from "react";
 import { useQuery } from "@apollo/client";
 
 import GET_PHOTOS from "./queries/getPhotos";
